8cf91a56e2f346c6ece2c46ffd5a5f6b33997a5f
shishir-uxl <shishirthapa521@gmail.com>
2022-09-01 11:08:06 +0545

    form handle in flask

--- views/view.py
+++ views/view.py
@@ -1,3 +1,4 @@
+import email
 import flask
 
 from web_app import app
@@ -9,5 +10,16 @@ def home():
 
 
 @app.route("/appointments")
-def add():
+def search():
     return flask.render_template("appointments.html")
+
+@app.route("/appointment/add")
+def add_get():
+    return flask.render_template("add.html", email = None, password = None)
+
+@app.route("/appointment/add", methods=["POST"])
+def add_post():
+    email = flask.request.form["email"]
+    password = flask.request.form["password"]
+    return flask.render_template("add.html", email = email, password = password )
+
