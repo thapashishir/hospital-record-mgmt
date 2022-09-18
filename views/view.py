ec1dafe37ae8e9dfc3b46d28df3b9770db134417
shishir-uxl <shishirthapa521@gmail.com>
2022-09-18 18:36:38 +0545

    search feature

--- views/view.py
+++ views/view.py
@@ -1,4 +1,3 @@
-from urllib import request
 import flask
 
 from web_app import app, get_db_conection
@@ -32,8 +31,37 @@ def home():
 
 
 @app.route("/appointments")
-def search():
+def appointment_list():
     return flask.redirect("/")
+
+@app.route("/appointments/search", methods = ["POST"] )
+def appointment_search():
+    sql = f"""
+    SELECT id, patient_name, doctor_name, appointment_date 
+    FROM appointments
+    WHERE patient_name like '%{flask.request.form["patient_name"]}%';
+    """
+    db_conn = get_db_conection()
+    result = db_conn.execute(sql).fetchall()
+    result_dict = []
+
+    #transforming result to result_dict
+    for row in result:
+        result_dict.append(
+            {
+                "id" : row[0],
+                "patient_name" : row[1],
+                "doctor_name" : row[2],
+                "appointment_date" : row[3],
+
+            }
+        )
+   
+    db_conn.close()
+    return flask.render_template(
+        "appointments.html", 
+        appointments = result_dict
+    )
 
 @app.route("/appointment/add")
 def add_get():
@@ -55,7 +83,6 @@ def add_post():
     #saving inserted data in database
     db_conn.commit()
     db_conn.close()
-
     return flask.redirect("/appointments")
 
 @app.route("/appointment/edit/<id>")
