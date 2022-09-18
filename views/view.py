38ddf4362ad35ab6548de76e0ed6328d1003fdbc
shishir-uxl <shishirthapa521@gmail.com>
2022-09-18 18:10:25 +0545

    edit and update feature

--- views/view.py
+++ views/view.py
@@ -1,3 +1,4 @@
+from urllib import request
 import flask
 
 from web_app import app, get_db_conection
@@ -36,7 +37,7 @@ def search():
 
 @app.route("/appointment/add")
 def add_get():
-    return flask.render_template("add.html", email = None, password = None)
+    return flask.render_template("add.html")
 
 @app.route("/appointment/add", methods=["POST"])
 def add_post():
@@ -57,3 +58,59 @@ def add_post():
 
     return flask.redirect("/appointments")
 
+@app.route("/appointment/edit/<id>")
+def edit_get(id):
+    db_conn = get_db_conection()
+    sql = f"""
+    SELECT * from appointments
+    WHERE id = {id}
+    """
+    result = db_conn.execute(sql).fetchone()
+    data = {
+        "id": result[0],
+        "patient_name": result[1],
+        "doctor_name": result[2],
+        "appointment_date": result[3]
+    }
+    db_conn.close()
+    return flask.render_template(
+        "edit.html", 
+        appointment = data
+    )
+
+
+@app.route("/appointment/edit", methods=["POST"])
+def edit_post():
+    db_conn = get_db_conection()
+    request_data = {
+        "id" : flask.request.form["id"],
+        "patient_name": flask.request.form["patient_name"],
+        "doctor_name": flask.request.form["doctor_name"],
+        "appointment_date": flask.request.form["appointment_date"]
+    }
+    sql = f"""
+    UPDATE appointments
+    SET 
+    patient_name = '{request_data['patient_name']}',
+    doctor_name = '{request_data["doctor_name"]}',
+    appointment_date = '{request_data["appointment_date"]}'
+    WHERE id = {request_data["id"]}
+    """
+    print(sql)
+    db_conn.execute(sql)
+    db_conn.commit()
+    db_conn.close()
+    return flask.redirect("/appointments")
+
+
+@app.route("/appointment/delete/<id>", methods=["GET"])
+def delete_get(id):
+    db_conn = get_db_conection()
+    sql = f"""
+    DELETE FROM appointments
+    WHERE id = {id}
+    """
+    db_conn.execute(sql)
+    db_conn.commit()
+    db_conn.close()
+    return flask.redirect("/appointments")
